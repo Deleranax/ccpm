90f711201c95fd7ce705d79863603d505c7ea572
Alexandre Leconte <aleconte@dwightstudio.fr>
2026-01-22 17:30:48 +0100

    Adding compatibility to CraftOS-PC

--- packages/libccpm/source/startup/00-ccpm-require.lua
+++ packages/libccpm/source/startup/00-ccpm-require.lua
@@ -21,11 +21,6 @@ if os.isCCPM then
     return
 end
 
-local CUSTOM_PACKAGE_PATH = "/lib/?;/lib/?.lua;/lib/?/init.lua;"
-
--- Exit the shell at the end of the program.
-shell.exit()
-
 --- Restart the shell to continue normal execution
 local function continue_execution()
     term.clear()
@@ -41,6 +36,22 @@ local function continue_execution()
     os.run({}, "rom/programs/shutdown.lua")
 end
 
+--- Confirm the presence of CCPM
+local function replace_version()
+    -- Replace the version string
+    local oldversion = os.version
+    os.version = function()
+        return oldversion() .. " (with CCPM)"
+    end
+
+    -- Add custom flag
+    os.isCCPM = true
+end
+
+local CUSTOM_PACKAGE_PATH = "/lib/?;/lib/?.lua;/lib/?/init.lua;"
+
+-- Exit the shell at the end of the program.
+shell.exit()
 
 -- Replace the default shell.run to skip the other startup
 shell.run = function(...) end
@@ -51,41 +62,53 @@ os.run = function(...)
     os.run = oldrun
     package.path = CUSTOM_PACKAGE_PATH .. package.path
 
-    -- Replace the defaults functions here
-    local oldloadfile = loadfile
-    _G.loadfile = function(filename, mode, env)
-        if filename:find("cc/require%.lua$") then
-            local content = ""
+    -- Patch flag
+    local patched = false
 
-            -- Load the original file
-            do
-                local file = fs.open(filename, "r")
-                content = file.readAll()
-                file.close()
-            end
+    -- Verify if we can do it the "right way"
+    if _HOST then
+        if string.match(_HOST, "CraftOS%-PC") then
+            -- Modify the package path
+            settings.set(
+                "shell.package_path",
+                package.path
+            )
 
-            -- Inject new paths
-            content = content:gsub("package%.path = \"", "package.path = \"" .. CUSTOM_PACKAGE_PATH)
-
-            local fun, err = loadstring(content, "@/" .. filename)
-            if not fun then
-                error(err)
-            end
-            setfenv(fun, env)
-
-            return fun
-        else
-            return oldloadfile(filename, mode, env)
+            patched = true
         end
     end
 
-    local oldversion = os.version
-    os.version = function()
-        return oldversion() .. " (with CCPM)"
+    -- If not, let hack our way into CraftOS
+    if not patched then
+        -- Replace the defaults functions here
+        local oldloadfile = loadfile
+        _G.loadfile = function(filename, mode, env)
+            if filename:find("cc/require%.lua$") then
+                local content = ""
+
+                -- Load the original file
+                do
+                    local file = fs.open(filename, "r")
+                    content = file.readAll()
+                    file.close()
+                end
+
+                -- Inject new paths
+                content = content:gsub("package%.path = \"", "package.path = \"" .. CUSTOM_PACKAGE_PATH)
+
+                local fun, err = loadstring(content, "@/" .. filename)
+                if not fun then
+                    error(err)
+                end
+                setfenv(fun, env)
+
+                return fun
+            else
+                return oldloadfile(filename, mode, env)
+            end
+        end
     end
 
-    -- Add custom flag
-    os.isCCPM = true
-
+    replace_version()
     continue_execution()
 end
