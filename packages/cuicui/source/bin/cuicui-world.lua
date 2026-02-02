65c678a7ad31a2a08897112f07cf78c978e7e5c7
Alexandre Leconte <aleconte@dwightstudio.fr>
2026-02-02 15:03:46 +0100

    Update cuicui-world.lua

--- packages/cuicui/source/bin/cuicui-world.lua
+++ packages/cuicui/source/bin/cuicui-world.lua
@@ -16,7 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses>.
 --]]
 
---! no-minify
+--! preserve-lines
 
 local cuicui  = require("cuicui")
 local const   = require("cuicui.const")
